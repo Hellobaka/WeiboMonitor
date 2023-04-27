45363b50a1f504ffdaba4e5e099afe75df575073
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2023-04-27 13:42:53 +0800

    测试器

---
+++ Tester/Program.cs
@@ -0,0 +1,18 @@
+﻿using WeiboMonitor.API;
+
+namespace Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetTimeLine timeLine = new GetTimeLine(1842706721);
+            //timeLine.GetTimeLineList();
+            var item = timeLine.CheckUpdate();
+            if (item != null)
+            {
+                timeLine.DownloadPic(item);
+            }
+        }
+    }
+}
